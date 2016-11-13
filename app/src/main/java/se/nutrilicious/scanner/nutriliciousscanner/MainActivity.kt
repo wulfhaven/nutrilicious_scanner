96137e86653f515aeda83f5ee657ccaaa8a2471d
Ulvhamne <wulfhaven@gmail.com>
2016-11-13 15:47:42 +0100

    Added anko

--- app/src/main/java/se/nutrilicious/scanner/nutriliciousscanner/MainActivity.kt
+++ app/src/main/java/se/nutrilicious/scanner/nutriliciousscanner/MainActivity.kt
@@ -7,19 +7,20 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
+        setSupportActionBar(find<Toolbar>(R.id.toolbar))
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        with(find<FloatingActionButton>(R.id.fab)) {
+            setOnClickListener { view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+            }
         }
     }
 
